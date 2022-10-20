084669d3d2d81c04e531efb57ab845df4e43fc85
hanSomBorISKogen <joel-aannmo@hotmail.com>
2022-10-20 21:04:10 +0200

    Fixed with dialogs!

---
+++ Othello/Views/DrawnDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace OthelloPresentation.Views
+{
+    /// <summary>
+    /// Interaction logic for DrawDialog.xaml
+    /// </summary>
+    public partial class DrawnDialog : Window
+    {
+        public DrawnDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
